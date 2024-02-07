cd5d56231e5366356676b62b74cf1ac02a9d4767
Phong Nguyen <phrongnguyn@gmail.com>
2024-02-07 15:44:29 -0500

    Hong Phong Nguyen

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -3,6 +3,7 @@ package com.college.converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,20 +25,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
+    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i(TAG, "Starting onCreate()");
         Button buttonConvert = findViewById(R.id.convertButton);
 
         buttonConvert.setOnClickListener( view ->  {
             convertCurrency(view);
         } );
+        Log.i(TAG, "Ending onCreate()");
     }
 
     public void convertCurrency(View view) {
-
+        Log.i(TAG, "Entering convertCurrency()");
         EditText inputView = findViewById(R.id.entryId);
 
         String inputAmount = inputView.getText().toString();
@@ -51,5 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
             resultView.setText( resultFloat + " Euros" );
         }
+        Log.i(TAG, "Exiting convertCurrency() successfully");
     }
 }
